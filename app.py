dca29526170de73d77bbfe86a98dd0796e167a37
Agon Cecelia <agonn.c@gmail.com>
2020-01-21 17:37:00 +0100

    cka me shkru

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, json
 from flask_pymongo import PyMongo
 from bson import json_util
-
+from bson.objectid import ObjectId
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/issues"
 mongo = PyMongo(app)
@@ -17,3 +17,22 @@ def find():
     result = list(mongo.db.issues.find())
     print(result)
     return json.dumps(result, default=json_util.default)
+
+@app.route('/issue/<id>', methods=["DELETE"])
+def delete(id):
+    print(id)
+    result = mongo.db.issues.delete_one({"_id": ObjectId(id)})
+
+    response = {}
+    if result.deleted_count > 0:
+        response["success"] = True
+        return jsonify(response)
+    return jsonify({"success": False, "message": "Not found", "status": 404})
+
+
+@app.route('/issue/<id>',methods=["PUT"])
+def update(id):
+#    db.sample.update({"_id":"1002"},{"$set":{"city":"Visakhapatnam"}})
+    result = mongo.db.issues.update_one({"_id":ObjectId(id)},{"$set":{"resolved":True}})
+    print(result)
+    return jsonify({'success': True})
